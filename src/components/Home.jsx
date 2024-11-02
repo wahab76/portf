d02deaa88de7e59a00d149ece7e90097842df82d
wahab76 <99541054+wahab76@users.noreply.github.com>
2024-11-02 13:42:50 +0530

    Update Home.jsx
    h

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="flex h-screen md:flex-row flex-col items-center justify-around" id="home">
+    <div className="flex md:h-screen md:flex-row flex-col items-center justify-around" id="home">
         <div className="hl md:w-[50%] md:p-0 p-2">
             <h1 className="text-4xl md:text-6xl font-semibold">ABDUL WAHAB REHAN</h1>
             <p className="mt-2 text-lg md:text-2xl">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, totam placeat at, minus ratione provident nulla in labore cumque ipsum maxime aliquam alias ducimus numquam quaerat blanditiis. At inventore, ex impedit doloremque ad, odit, repellendus eius nostrum eligendi dolorum assumenda?</p>
