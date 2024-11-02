313e0e12c6dca278cfcb5a1d5371fab03e06651a
wahab76 <99541054+wahab76@users.noreply.github.com>
2024-11-02 13:42:17 +0530

    Update About.jsx
    height

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className="flex flex-col md:flex-row justify-around items-center h-screen" id="about">
+    <div className="flex flex-col md:flex-row justify-around items-center md:h-screen" id="about">
         <div className="al md:w-[50%]">
             <h1 className="text-3xl md:text-5xl font-medium">About Me</h1>
             <p className="text-lg md:text-2xl mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt odio, reprehenderit facilis quod fugit illo maxime? Iusto velit fuga maxime laboriosam, illo repudiandae odio, facere, error quam a autem nihil aperiam hic et aliquam porro rerum fugit numquam dolorum consequuntur! Sit adipisci accusamus exercitationem ab molestias obcaecati, magni impedit quos?</p>
